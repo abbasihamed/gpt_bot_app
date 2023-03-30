f9948d1070c2dc9c30618f55c4f7bb4f97e0fb3d
omnimanager <hamed137881@gmail.com>
2023-03-30 14:18:27 +0330

    add .env

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String apiKey = '';
-
 DateTime dateTime = DateTime.now();
 
-GlobalKey<ScaffoldMessengerState> snackBar = GlobalKey<ScaffoldMessengerState>();
+GlobalKey<ScaffoldMessengerState> snackBar =
+    GlobalKey<ScaffoldMessengerState>();
